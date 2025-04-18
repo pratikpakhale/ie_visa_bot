f35c22ab14ad6d2d01b2595bb0b109588b13731d
pratik pakhale <pratikpakhale20@gmail.com>
2025-04-19 01:38:42 +0530

    chore: update application numbers

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 APPLICATIONS = {
-    "73945462": "Pratik",
 #    "54834512": "Sample"
+    "73945462": "Pratik",
+    "74836482": "Vijaya",
+    "74836142": "Vilas",
+    "74835652": "Vikas",
+    "74835872": "Sunita"
 }
